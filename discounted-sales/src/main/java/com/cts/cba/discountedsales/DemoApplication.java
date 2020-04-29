4b857fa0afaf281c65e21c534ee8a82bc2a3e735
rishabh_nag <Rishabh.Nag@cognizant.com>
2020-04-30 01:59:29 +0530

    Initial commit

---
+++ discounted-sales/src/main/java/com/cts/cba/discountedsales/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.cts.cba.discountedsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
